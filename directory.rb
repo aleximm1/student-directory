2e9b099171d90384d5e7d62b237a28d81054567b
Alex Imm <alexanderimm@hotmail.co.uk>
2017-02-02 13:21:03 +0000

    methods created

--- directory.rb
+++ directory.rb
@@ -12,12 +12,21 @@ students = [
   "Joffrey Baratheon"
   "Norman Bates"
 ]
-puts "The students of Villains Academy"
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------"
+end
 #print the list of students each on a new line
-puts "--------------"
-students.each do |student|
-  puts student
+def print_names(names)
+  names.each do |name|
+    puts name
+  end
 end
 #print the total number of students
-# it's important that print() doesn't add new line characters
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+#call the methods
+print_header
+print_names(students)
+print_footer(students)
