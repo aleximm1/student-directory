0455768c4926dfbcff697a34f91d2cd534d8730a
Alex Imm <alexanderimm@hotmail.co.uk>
2017-02-09 15:32:41 +0000

    saving data to file done

--- directory.rb
+++ directory.rb
@@ -69,7 +69,6 @@ def input_students
     if cohort = ""
       cohort = "november"
     end
-    cohort.to_sym
     # add more info to the students (exercise 5)
     puts "Hobbies:"
     hobbies = gets.chomp
@@ -94,6 +93,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. Show students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -114,6 +114,8 @@ def process(selection)
       input_students
     when "2"
         show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -127,5 +129,18 @@ def interactive_menu
     process(gets.chomp)
   end
 end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:hobbies], student[:country], student[:height]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 # call the methods
 interactive_menu
